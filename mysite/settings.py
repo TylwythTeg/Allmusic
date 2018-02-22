11c91090c301a76e2ebfb007669e05e4cbc60d7c
TylwythTeg <garciarobertoiv@yahoo.com>
2018-02-22 00:30:44 -0800

    Made the menus collapsable now. They're somewhat limited to just that one collapse point, but that might be fine because the categories are broad right now

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'efu4q##vvx3q^bez6kbsckfx(5z0-sk%lbrm6+^32m2wi=mnlx'
 DEBUG = True
 
 #Hosts I'm allowing
-ALLOWED_HOSTS = ["f06ffa13.ngrok.io", "127.0.0.1"]
+ALLOWED_HOSTS = ["0c197815.ngrok.io", "127.0.0.1"]
 
 
 # Application definition
--- mysite/static/js/sidebar.js
+++ mysite/static/js/sidebar.js
@@ -4,11 +4,33 @@ document.addEventListener("DOMContentLoaded", function() {
     var sidebar = document.getElementById("chord-scale-sidebar");
     var callback = function (event) {
         sidebar.classList.toggle("visible");   
-    }
+    };
     clickable.addEventListener("click", callback);
 
     var close = document.getElementById("close-menu-button");
     close.addEventListener("click", callback);
+
+
+
+
+
+
+
+    /* This is the collapsable submenus */
+    var menu = document.getElementsByClassName("main-menu")[0];
+    var submenus = document.getElementsByClassName("menu-level-2");
+    var items = document.getElementsByClassName("menu-level-3");
+
+    var unfurl = function(event) {
+        if(!event.target.parentElement.classList.contains("menu-level-2")) {
+            return;
+        }
+        event.target.parentElement.nextElementSibling.classList.toggle("items-visible");
+        event.stopPropogation();
+    };
+    menu.addEventListener("click",unfurl);
+    /*  This is the collapsable submenus */
+
 
 });
 
