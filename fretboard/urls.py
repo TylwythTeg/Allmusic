a9e03b4caa21d7470575aa13caaddcf8f4daaf5b
TylwythTeg <garciarobertoiv@yahoo.com>
2018-02-22 02:17:28 -0800

    I've got some basic-testing-out reverse url matching going on the diatonic scale so you can move around within the note you put in the URL

--- fretboard/urls.py
+++ fretboard/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     #url(r'^$', views.test, name='test'),
     url(r'^$', views.index, name='index'),
     url(r'^(scale)/(?P<scale_type>[\w|\W]+)/(?P<root>[\w|\W]+)/', views.scale, name = 'scale'),
+    url(r'^(?P<scale_type>[\w|\W]+)/(?P<root>[\w|\W]+)/', views.scale, name = 'fretboardscale'),
     #url(r'^(chord)/(triad)/(?P<triad_type>[\w|\W]+)/(?P<root>[\w|\W]+)/(?P<sub_chord>[\w|\W]+)/', views.subchord, name = 'subchord'),
     #url(r'^(chord)/(triad)/(?P<triad_type>[\w|\W]+)/(?P<root>[\w|\W]+)/', views.triad, name = 'triad'),
     #url(r'^(?P<scale_type>[\w|\W]+)/', views.index, name = 'index'),
